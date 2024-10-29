ae10692fe433402f8d7b92c52315f4568855415b
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-10-30 01:17:53 +0530

    prog 7

---
+++ prog 7/README.md
@@ -0,0 +1,7 @@
+# Output
+
+Server ready for connection
+Connection is successful and wating for chatting
+
+Enter the file name test.txt
+Hello
---
+++ prog 7/src/Lclient.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+
+public class Lclient {
+    public static void main(String args[]) throws Exception {
+        try (Socket sock = new Socket("127.0.0.1", 4000)) {
+            // reading the file name from keyboard. Uses input stream
+            System.out.print("Enter the file name");
+            BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+            String fname = keyRead.readLine(); // sending the file name to server. Uses PrintWriter
+            try {
+                OutputStream ostream = sock.getOutputStream();
+                PrintWriter pwrite = new PrintWriter(ostream, true);
+                pwrite.println(fname); // receiving the contents from server. Uses input stream
+                InputStream istream = sock.getInputStream();
+                BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream));
+                String str;
+                while ((str = socketRead.readLine()) != null) // reading line-by-line
+                {
+                    System.out.println(str);
+                }
+                pwrite.close();
+                socketRead.close();
+                keyRead.close();
+            } catch (Exception e) {
+                System.out.println("An error occured while opening the file");
+            }
+        }
+    }
+}
